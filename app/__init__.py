131a72d37c4a7d33a3b2226426d40cfabce63222
Sam Smith <sam@example.com>
2020-10-18 21:44:10 +0300

    initialized my app

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,13 @@ from config import config_options
 bootstrap = Bootstrap()
 
 
-def create_app(config_name):
 
+
+def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-
     app.config.from_object(config_options[config_name])
-    # config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
@@ -21,26 +20,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
-    
+    from .request import configure_request
+    configure_request(app)
+
     return app
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
