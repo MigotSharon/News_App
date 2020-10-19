23fa1437da86ce692904ccf44bfec292ed50f20c
Sam Smith <sam@example.com>
2020-10-19 11:34:29 +0300

    moved files from my app folder into my tests folder

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template, request
+from . import main
+from app.request import get_sources, get_articles
+
+
+@main.route('/')
+def index():
+    sources = get_sources()
+    #print(sources)
+    return render_template('index.html', sources=sources)
+
+
+@main.route('/article/<id>')
+def source_article(id):
+    source_articles = get_articles(id)
+    return render_template('articles_display.html', source_articles=source_articles)
